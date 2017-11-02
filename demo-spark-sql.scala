347a609bd1db333f905504e5e3ee648c5af1c86c
fclesio <flavioclesio@yahoo.com>
2017-11-02 15:33:31 -0200

    Examples with group by, global view, new reference for file to load

--- demo-spark-sql.scala
+++ demo-spark-sql.scala
@@ -1,16 +1,20 @@
 ////////////////
-/Carga de dados
+//Carga de dados
 ////////////////
+
+//Imports para usar as funções de sort e para que hajam as conversões implícitas para trabalhar com os integers 
+import org.apache.spark.sql.functions._
+import spark.implicits._
 
 //Carga do arquivo jSON para um dataframe
 val df = spark.read.json("examples/src/main/resources/people.json")
+//val df = spark.read.json("/Volumes/PANZER/Github/spark-cases/people.json")
 
 //Vamos ver o que tem dentro do nosso arquivo jSON
 df.show()
 
 //Para checar os datatypes, basta usar o método do dataframe chamado .printSchema()
 df.printSchema()
-
 
 
 
@@ -27,10 +31,16 @@ df.select($"name", $"age", $"age" + 1).show()
 //Filtra pessoas com mais de 21 anos
 df.filter($"age" > 21).show()
 
+//VVamos filtrar as pessoas com mais de 21 anos, e com idade igual ou menor do que 35
+df.filter($"age" > 21 && $"age" <= 35).show()
+
 //Agrupa via contagem todas as pessoas pela idade
 df.groupBy("age").count().show()
 
+//Formas de realizar ordenação
+df.groupBy("age").count().orderBy(desc("count")).show()
 
+df.groupBy("age").count().sort($"count".desc).show()
 
 
 
